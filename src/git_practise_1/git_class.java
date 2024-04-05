41ec38a0ae687218246f429571a009299e79cb66
kusha1990 <cg.kusha@gmail.com>
2024-04-05 12:53:40 +0530

    kusha first commit

---
+++ src/git_practise_1/git_class.java
@@ -0,0 +1,10 @@
+package git_practise_1;
+
+public class git_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("this is my git");
+	}
+
+}
